58428ce403f023ce01b6eee6dd1efa7786ca6013
Faiz Shaikh <faizalam2432005@gmail.com>
2023-08-10 22:51:10 +0530

    All features Completed

---
+++ src/contexts/FakeAuthContext.jsx
@@ -0,0 +1,58 @@
+import { createContext, useContext, useReducer } from "react";
+
+const AuthContext = createContext();
+
+const initialState = {
+  user: null,
+  isAuthenticated: false,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "login":
+      return { ...state, user: action.payload, isAuthenticated: true };
+    case "logout":
+      return { ...state, user: null, isAuthenticated: false };
+    default:
+      throw new Error("Unknown action");
+  }
+}
+
+const FAKE_USER = {
+  name: "Faiz",
+  email: "faiz@example.com",
+  password: "qwerty",
+  avatar:
+    "https://media.licdn.com/dms/image/D4D03AQHyDjvzCocTRw/profile-displayphoto-shrink_400_400/0/1673930032046?e=1697068800&v=beta&t=epChOB91vOPNKGiS3ZnnhjZ27Betcv5agjck3Gds4xA",
+};
+
+function AuthProvider({ children }) {
+  const [{ user, isAuthenticated }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  function login(email, password) {
+    if (email === FAKE_USER.email && password === FAKE_USER.password)
+      dispatch({ type: "login", payload: FAKE_USER });
+  }
+
+  function logout() {
+    dispatch({ type: "logout" });
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+function useAuth() {
+  const context = useContext(AuthContext);
+  if (context === undefined)
+    throw new Error("AuthContext was used outside AuthProvider");
+  return context;
+}
+
+export { AuthProvider, useAuth };
